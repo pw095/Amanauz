9afc71036710335b6826c7d0e4e79baf045fa830
Pavel Gnezdilov <pw095@fnbic.ru>
2021-04-01 21:30:28 +0300

    Исправить DDL таблицы tbl_elm_dependency

--- db/script/meta/model/tbl_elm_dependency.sql
+++ db/script/meta/model/tbl_elm_dependency.sql
@@ -2,9 +2,9 @@ DROP TABLE tbl_elm_dependency;
 CREATE TABLE tbl_elm_dependency
   (
     elmd_id            INTEGER PRIMARY KEY NOT NULL,
-    elmd_parent_elm_id INTEGER             NOT NULL,
-    elmd_child_elm_id  INTEGER             NOT NULL,
-    UNIQUE(elmd_parent_elm_id, elmd_child_elm_id),
-    FOREIGN KEY (elmd_parent_elm_id) REFERENCES tbl_entity_layer_map(elm_id) ON DELETE CASCADE,
-    FOREIGN KEY (elmd_child_elm_id)  REFERENCES tbl_entity_layer_map(elm_id) ON DELETE CASCADE
+    elmd_elm_id        INTEGER             NOT NULL,
+    elmd_parent_elm_id INTEGER,
+    UNIQUE(elmd_elm_id, elmd_parent_elm_id),
+    FOREIGN KEY (elmd_elm_id)        REFERENCES tbl_entity_layer_map(elm_id) ON DELETE CASCADE,
+    FOREIGN KEY (elmd_parent_elm_id) REFERENCES tbl_entity_layer_map(elm_id) ON DELETE CASCADE
   );
