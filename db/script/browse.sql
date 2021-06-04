528fd32455ca0e7e8bc24193c5b14a8c9292bb51
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-04 20:44:50 +0300

    Редактировать browse.sql

--- db/script/browse.sql
+++ db/script/browse.sql
@@ -101,3 +101,70 @@ SELECT * FROM pragma_index_info('tbl_entity');
 SELECT * FROM pragma_analysis_limit(-1);
 PRAGMA analysis_limit=-1;
 select t.* from sqlite_master m, pragma_index_list(m.name) t, pragma_index_info;
+
+
+
+WITH
+     w_raw AS
+     (
+         SELECT
+                security_id,
+                board_id,
+                face_value,
+                previous_date,
+                sha1(concat_value) AS sha1_value,
+                load_dt
+           FROM (SELECT
+                        security_id,
+                        board_id,
+                        face_value,
+                        previous_date,
+                        '_' || IFNULL(CAST(face_value    AS TEXT), '!@#$%^&*') ||
+                        '_' || IFNULL(CAST(previous_date AS TEXT), '!@#$%^&*') || '_' AS concat_value,
+                        load_dt
+                   FROM (SELECT
+                                security_id,
+                                board_id,
+                                face_value,
+                                previous_date,
+                                load_dt,
+                                ROW_NUMBER() OVER (PARTITION BY security_id, board_id, load_dt
+                                                       ORDER BY load_dttm DESC)                AS rn
+                           FROM (SELECT
+                                        data.security_id,
+                                        data.board_id,
+                                        data.face_value,
+                                        data.previous_date,
+                                        DATE(tech$load_dttm) AS load_dt,
+                                        tech$load_dttm       AS load_dttm
+                                   FROM security_daily_info data))
+                  WHERE rn = 1)
+     )
+SELECT
+       security_id,
+       board_id,
+       face_value,
+       previous_date,
+       load_dt                                                                                 AS tech$effective_dt,
+       LEAD(DATE(load_dt, '-1 DAY'), 1, '2999-12-31') OVER (PARTITION BY security_id, board_id
+                                                                ORDER BY load_dt)              AS tech$expiration_dt,
+       sha1_value
+  FROM (SELECT
+               security_id,
+               board_id,
+               face_value,
+               previous_date,
+               sha1_value,
+               load_dt
+          FROM (SELECT
+                       security_id,
+                       board_id,
+                       face_value,
+                       previous_date,
+                       sha1_value,
+                       LAG(sha1_value) OVER (PARTITION BY security_id, board_id
+                                                 ORDER BY load_dt)              AS lag_sha1_value,
+                       load_dt
+                  FROM w_raw)
+         WHERE sha1_value != lag_sha1_value
+            OR lag_sha1_value IS NULL);
