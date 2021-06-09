c7b047f848f8103f0c5f4845e0529880c6c5a4e9
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-09 22:24:27 +0300

    Отредактировать скрипт загрузки в техническую таблицу. Добавить WINDOW clause.

--- db/script/etl/repl/index_security_weight/sql/full/tech$index_security_weight.sql
+++ db/script/etl/repl/index_security_weight/sql/full/tech$index_security_weight.sql
@@ -12,12 +12,12 @@ INSERT
     security_id,
     weight,
     trading_session
-  )
+  );
 WITH
      w_raw AS
      (
          SELECT
-                load_dt,
+                tech$load_dt,
                 sha1(concat_value) AS hash_value,
                 index_id,
                 trade_date,
@@ -27,7 +27,7 @@ WITH
                 weight,
                 trading_session
            FROM (SELECT
-                        load_dt,
+                        tech$load_dt,
                         '_' || IFNULL(CAST(ticker          AS TEXT), '!@#$%^&*') ||
                         '_' || IFNULL(CAST(short_name      AS TEXT), '!@#$%^&*') ||
                         '_' || IFNULL(CAST(weight          AS TEXT), '!@#$%^&*') ||
@@ -40,12 +40,8 @@ WITH
                         weight,
                         trading_session
                    FROM (SELECT
-                                load_dt,
-                                ROW_NUMBER() OVER (PARTITION BY index_id,
-                                                                trade_date,
-                                                                security_id,
-                                                                load_dt
-                                                       ORDER BY load_dttm DESC) AS rn,
+                                tech$load_dt,
+                                ROW_NUMBER() OVER (wnd) AS rn,
                                 index_id,
                                 trade_date,
                                 security_id,
@@ -54,11 +50,8 @@ WITH
                                 weight,
                                 trading_session
                            FROM (SELECT
-                                        sha1(CAST(index_id    AS TEXT) ||
-                                             CAST(trade_date  AS TEXT) ||
-                                             CAST(security_id AS TEXT))   AS hash_key,
-                                        DATE(tech$load_dttm)              AS load_dt,
-                                        tech$load_dttm                    AS load_dttm,
+                                        DATE(tech$load_dttm) AS tech$load_dt,
+                                        tech$load_dttm       AS tech$load_dttm,
                                         index_id,
                                         trade_date,
                                         security_id,
@@ -67,17 +60,19 @@ WITH
                                         weight,
                                         trading_session
                                    FROM index_security_weight
-                                  WHERE tech$load_id >= :tech$load_id))
+                                  WHERE tech$load_id >= :tech$load_id)
+                         WINDOW wnd AS (PARTITION BY index_id,
+                                                     trade_date,
+                                                     security_id,
+                                                     tech$load_dt
+                                            ORDER BY tech$load_dttm DESC))
                   WHERE rn = 1)
      )
 SELECT
-       1                                                                             AS tech$load_id,
-       load_dt                                                                       AS tech$effective_dt,
-       LEAD(DATE(load_dt, '-1 DAY'), 1, '2999-12-31') OVER (PARTITION BY index_id,
-                                                                         trade_date,
-                                                                         security_id
-                                                                ORDER BY load_dt)    AS tech$expiration_dt,
-       hash_value                                                                    AS tech$hash_value,
+       1                                                              AS tech$load_id,
+       tech$load_dt                                                   AS tech$effective_dt,
+       LEAD(DATE(tech$load_dt, '-1 DAY'), 1, '2999-12-31') OVER (wnd) AS tech$expiration_dt,
+       hash_value                                                     AS tech$hash_value,
        index_id,
        trade_date,
        security_id,
@@ -86,7 +81,7 @@ SELECT
        weight,
        trading_session
   FROM (SELECT
-               load_dt,
+               tech$load_dt,
                hash_value,
                index_id,
                trade_date,
@@ -96,12 +91,9 @@ SELECT
                weight,
                trading_session
           FROM (SELECT
-                       load_dt,
+                       tech$load_dt,
                        hash_value,
-                       LAG(hash_value) OVER (PARTITION BY index_id,
-                                                          trade_date,
-                                                          security_id
-                                                 ORDER BY load_dt) AS lag_hash_value,
+                       LAG(hash_value) OVER (wnd) AS lag_hash_value,
                        index_id,
                        trade_date,
                        security_id,
@@ -109,6 +101,14 @@ SELECT
                        short_name,
                        weight,
                        trading_session
-                  FROM w_raw)
+                  FROM w_raw
+                WINDOW wnd AS (PARTITION BY index_id,
+                                            trade_date,
+                                            security_id
+                                   ORDER BY tech$load_dt))
          WHERE hash_value != lag_hash_value
             OR lag_hash_value IS NULL)
+WINDOW wnd AS (PARTITION BY index_id,
+                            trade_date,
+                            security_id
+                   ORDER BY tech$load_dt)
