896201315ce24313364d61fb6e6a8c9106682b11
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-14 13:48:17 +0300

    Добавить сущность security_board_groups в слой REPL

---
+++ db/script/etl/repl/security_board_groups/model/tech$security_board_groups.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS tech$security_board_groups;
+CREATE TABLE tech$security_board_groups
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$effective_dt  TEXT    NOT NULL,
+    tech$expiration_dt TEXT    NOT NULL,
+    tech$hash_value    TEXT    NOT NULL,
+    id                 INTEGER NOT NULL,
+    trade_engine_id    INTEGER NOT NULL,
+    trade_engine_name  TEXT    NOT NULL,
+    trade_engine_title TEXT    NOT NULL,
+    market_id          INTEGER NOT NULL,
+    market_name        TEXT    NOT NULL,
+    name               TEXT    NOT NULL,
+    title              TEXT    NOT NULL,
+    is_default         INTEGER NOT NULL,
+    board_group_id     INTEGER NOT NULL,
+    is_traded          INTEGER NOT NULL,
+    PRIMARY KEY(board_group_id, tech$effective_dt)
+  )
+WITHOUT ROWID;
