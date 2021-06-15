4245c602b3f528e854d0c739199d489673bd7aa2
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-15 09:55:10 +0300

    Добавить сущность foreign_currency_dictionary в слой REPL

---
+++ db/script/etl/repl/foreign_currency_dictionary/model/foreign_currency_dictionary.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS foreign_currency_dictionary;
+CREATE TABLE foreign_currency_dictionary
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$effective_dt  TEXT    NOT NULL,
+    tech$expiration_dt TEXT    NOT NULL,
+    tech$hash_value    TEXT    NOT NULL,
+    id                 INTEGER NOT NULL,
+    name               INTEGER NOT NULL,
+    eng_name           TEXT    NOT NULL,
+    nominal            TEXT    NOT NULL,
+    parent_code        TEXT    NOT NULL,
+    iso_num_code       TEXT,
+    iso_char_code      TEXT,
+    PRIMARY KEY(id, tech$effective_dt)
+  )
+WITHOUT ROWID;
