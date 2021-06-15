8f0ea2e79785d887dd87e1f695bded254b6aad70
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-15 09:53:14 +0300

    Добавить сущность security_collections в слой REPL

---
+++ db/script/etl/repl/security_collections/model/security_boards.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS security_boards;
+CREATE TABLE security_boards
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$effective_dt  TEXT    NOT NULL,
+    tech$expiration_dt TEXT    NOT NULL,
+    tech$hash_value    TEXT    NOT NULL,
+    id                 INTEGER NOT NULL,
+    board_group_id     INTEGER NOT NULL,
+    engine_id          INTEGER NOT NULL,
+    market_id          INTEGER NOT NULL,
+    board_id           TEXT    NOT NULL,
+    board_title        TEXT    NOT NULL,
+    is_traded          INTEGER NOT NULL,
+    has_candles        INTEGER NOT NULL,
+    is_primary         INTEGER NOT NULL,
+    PRIMARY KEY(board_id, tech$effective_dt)
+  )
+WITHOUT ROWID;
