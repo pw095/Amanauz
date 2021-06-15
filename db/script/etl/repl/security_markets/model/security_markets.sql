d18634b2118fb4a20ef2b2f12bd10310210453b6
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-15 09:54:33 +0300

    Добавить сущность security_markets в слой REPL

---
+++ db/script/etl/repl/security_markets/model/security_markets.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS security_markets;
+CREATE TABLE security_markets
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$effective_dt  TEXT    NOT NULL,
+    tech$expiration_dt TEXT    NOT NULL,
+    tech$hash_value    TEXT    NOT NULL,
+    id                 INTEGER NOT NULL,
+    trade_engine_id    INTEGER NOT NULL,
+    trade_engine_name  TEXT    NOT NULL,
+    trade_engine_title TEXT    NOT NULL,
+    market_name        TEXT    NOT NULL,
+    market_title       TEXT    NOT NULL,
+    market_id          INTEGER NOT NULL,
+    market_place       INTEGER NOT NULL,
+    PRIMARY KEY(market_name, tech$effective_dt)
+  )
+WITHOUT ROWID;
