b009255527e0a3e363b63eebb146b01154902f6f
Pavel Gnezdilov <pw095@fnbic.ru>
2021-06-15 09:54:13 +0300

    Добавить сущность security_groups в слой REPL

---
+++ db/script/etl/repl/security_groups/model/security_groups.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS security_engines;
+CREATE TABLE security_engines
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$effective_dt  TEXT    NOT NULL,
+    tech$expiration_dt TEXT    NOT NULL,
+    tech$hash_value    TEXT    NOT NULL,
+    id                 INTEGER NOT NULL,
+    name               TEXT    NOT NULL,
+    title              TEXT    NOT NULL,
+    is_hidden          INTEGER NOT NULL,
+    PRIMARY KEY(name, tech$effective_dt)
+  )
+WITHOUT ROWID;
