e8f55a86533e0580fd42d0eb6ddce15e25b82555
Pavel Gnezdilov <pw095@fnbic.ru>
2021-07-05 23:11:43 +0300

    Исправить шаблон для генерации repl-скриптов

--- query_templates/Pattern.sql
+++ query_templates/Pattern.sql
@@ -89,4 +89,5 @@ SELECT
  WHERE 1 = 1
  ON CONFLICT(%businessKeyFields_singleLine%, tech$effective_dt)
  DO UPDATE
-       SET tech$expiration_dt = excluded.tech$expiration_dt
+       SET tech$expiration_dt = excluded.tech$expiration_dt,
+           tech$load_id = excluded.tech$load_id
