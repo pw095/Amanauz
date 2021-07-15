c00f262b17e5a63941f453e148c3ad9ad72873fa
Pavel Gnezdilov <pw095@fnbic.ru>
2021-07-15 22:10:49 +0300

    Исправить скрипт расчёта hash-суммы файла, загруженного на предыдущей итерации

--- db/script/meta/auxiliary/get_previous_file_hash.sql
+++ db/script/meta/auxiliary/get_previous_file_hash.sql
@@ -10,5 +10,10 @@ SELECT
                              NULL
                         FROM tbl_entity_load_log ell
                        WHERE ell.ell_id = fll.fll_ell_id
-                         AND ell.ell_elm_id = :ell_elm_id))
+                         AND ell.ell_elm_id = :ell_elm_id
+                         AND EXISTS(SELECT
+                                           NULL
+                                      FROM tbl_flow_log flow
+                                     WHERE flow.flow_id = ell.ell_flow_log_id
+                                       AND flow.flow_status = 'succeeded')))
  WHERE rn = 1
