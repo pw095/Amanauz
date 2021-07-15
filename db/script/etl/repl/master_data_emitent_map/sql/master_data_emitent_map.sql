58f82639bf859575aaca84eb6f4f4c13c884d0e4
Pavel Gnezdilov <pw095@fnbic.ru>
2021-07-15 18:43:04 +0300

    Добавить ETL-скрипты для расчёта файловых загрузок

---
+++ db/script/etl/repl/master_data_emitent_map/sql/master_data_emitent_map.sql
@@ -0,0 +1,107 @@
+INSERT
+  INTO master_data_emitent_map
+  (
+    tech$load_id,
+    tech$effective_dt,
+    tech$expiration_dt,
+    tech$hash_value,
+    source_system_code,
+    emitent_code,
+    emitent_short_name
+  )
+SELECT
+       :tech$load_id       AS tech$load_id,
+       tech$effective_dt,
+       tech$expiration_dt,
+       tech$hash_value,
+       source_system_code,
+       emitent_code,
+       emitent_short_name
+  FROM (SELECT
+               tech$effective_dt,
+               tech$expiration_dt,
+               tech$hash_value,
+               source_system_code,
+               emitent_code,
+               emitent_short_name
+          FROM tech$master_data_emitent_map src
+         WHERE NOT EXISTS(SELECT
+                                 NULL
+                            FROM master_data_emitent_map sat
+                           WHERE
+                                 sat.source_system_code = src.source_system_code
+                             AND sat.emitent_code = src.emitent_code
+                             AND sat.tech$expiration_dt = '2999-12-31')
+         UNION ALL
+        SELECT
+               CASE mrg.flg
+                    WHEN 'INSERT' THEN
+                        tech$effective_dt
+                    WHEN 'UPDATE' THEN
+                        tech$sat$effective_dt
+               END AS tech$effective_dt,
+               CASE mrg.flg
+                    WHEN 'INSERT' THEN
+                        tech$expiration_dt
+                    WHEN 'UPDATE' THEN
+                        tech$sat$expiration_dt
+               END AS tech$expiration_dt,
+               tech$hash_value,
+               source_system_code,
+               emitent_code,
+               emitent_short_name
+          FROM (SELECT
+                       tech$effective_dt,
+                       tech$expiration_dt,
+                       tech$sat$effective_dt,
+                       tech$sat$expiration_dt,
+                       tech$hash_value,
+                       source_system_code,
+                       emitent_code,
+                       emitent_short_name,
+                       CASE
+                            WHEN rn = 1
+                              OR rn = 2 AND fv_equal_flag = 'EQUAL' THEN
+                                'UPSERT'
+                            ELSE
+                                'INSERT'
+                       END AS upsert_flg
+                  FROM (SELECT
+                               src.tech$effective_dt,
+                               src.tech$expiration_dt,
+                               sat.tech$effective_dt                 AS tech$sat$effective_dt,
+                               DATE(src.tech$effective_dt, '-1 DAY') AS tech$sat$expiration_dt,
+                               src.tech$hash_value,
+                               src.source_system_code,
+                               src.emitent_code,
+                               src.emitent_short_name,
+                               FIRST_VALUE(CASE
+                                                WHEN src.tech$hash_value != sat.tech$hash_value THEN
+                                                    'NON_EQUAL'
+                                                ELSE
+                                                    'EQUAL'
+                                           END) OVER (wnd) AS fv_equal_flag,
+                               ROW_NUMBER() OVER (wnd)     AS rn
+                          FROM tech$master_data_emitent_map src
+                               JOIN
+                               master_data_emitent_map sat
+                                   ON
+                                      sat.source_system_code = src.source_system_code
+                                  AND sat.emitent_code = src.emitent_code
+                                  AND sat.tech$effective_dt < src.tech$effective_dt
+                                  AND sat.tech$expiration_dt = '2999-12-31'
+                        WINDOW wnd AS (PARTITION BY src.source_system_code,
+                                                    src.emitent_code
+                                           ORDER BY src.tech$effective_dt))
+                 WHERE rn = 1 AND fv_equal_flag = 'NON_EQUAL'
+                    OR rn > 1) src
+               CROSS JOIN
+               (SELECT 'INSERT' AS flg
+                 UNION ALL
+                SELECT 'UPDATE' AS flg) mrg
+    WHERE src.upsert_flg = 'UPSERT'
+       OR src.upsert_flg = 'INSERT' AND mrg.flg = 'INSERT')
+ WHERE 1 = 1
+ ON CONFLICT(source_system_code, emitent_code, tech$effective_dt)
+ DO UPDATE
+       SET tech$expiration_dt = excluded.tech$expiration_dt
---
+++ db/script/etl/repl/master_data_emitent_map/sql/tech$master_data_emitent_map.sql
@@ -0,0 +1,79 @@
+INSERT
+  INTO tech$master_data_emitent_map
+  (
+    tech$load_id,
+    tech$effective_dt,
+    tech$expiration_dt,
+    tech$hash_value,
+    source_system_code,
+    emitent_code,
+    emitent_short_name
+  )
+WITH
+     w_raw AS
+     (
+         SELECT
+                tech$load_dt,
+                sha1(concat_value) AS hash_value,
+                source_system_code,
+                emitent_code,
+                emitent_short_name
+           FROM (SELECT
+                        tech$load_dt,
+                        '_' || IFNULL(CAST(emitent_short_name AS TEXT), '!@#$%^&*') || '_' AS concat_value,
+                        source_system_code,
+                        emitent_code,
+                        emitent_short_name
+                   FROM (SELECT
+                                tech$load_dt,
+                                ROW_NUMBER() OVER (wnd) AS rn,
+                                source_system_code,
+                                emitent_code,
+                                emitent_short_name
+                           FROM (SELECT
+                                        DATE(tech$load_dttm) AS tech$load_dt,
+                                        tech$load_dttm       AS tech$load_dttm,
+                                        source_system_code,
+                                        emitent_code,
+                                        emitent_short_name
+                                   FROM src.master_data_emitent_map
+                                  WHERE tech$load_dttm > :tech$load_dttm)
+                         WINDOW wnd AS (PARTITION BY
+                                                     source_system_code,
+                                                     emitent_code,
+                                                     tech$load_dt
+                                            ORDER BY tech$load_dttm DESC))
+                  WHERE rn = 1)
+     )
+SELECT
+       :tech$load_id                                                  AS tech$load_id,
+       tech$load_dt                                                   AS tech$effective_dt,
+       LEAD(DATE(tech$load_dt, '-1 DAY'), 1, '2999-12-31') OVER (wnd) AS tech$expiration_dt,
+       hash_value                                                     AS tech$hash_value,
+       source_system_code,
+       emitent_code,
+       emitent_short_name
+  FROM (SELECT
+               tech$load_dt,
+               hash_value,
+               source_system_code,
+               emitent_code,
+               emitent_short_name
+          FROM (SELECT
+                       tech$load_dt,
+                       hash_value,
+                       LAG(hash_value) OVER (wnd) AS lag_hash_value,
+                       source_system_code,
+                       emitent_code,
+                       emitent_short_name
+                  FROM w_raw
+                WINDOW wnd AS (PARTITION BY
+                                            source_system_code,
+                                            emitent_code
+                                   ORDER BY tech$load_dt))
+         WHERE hash_value != lag_hash_value
+            OR lag_hash_value IS NULL)
+WINDOW wnd AS (PARTITION BY
+                            source_system_code,
+                            emitent_code
+                   ORDER BY tech$load_dt)
