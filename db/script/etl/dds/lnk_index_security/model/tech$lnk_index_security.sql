9f2e2cc6c9e5dcf5c170cfc0e283494a1930c1b9
Pavel Gnezdilov <pw095@fnbic.ru>
2021-08-24 09:49:53 +0300

    Добавить lnk_index_security

---
+++ db/script/etl/dds/lnk_index_security/model/tech$lnk_index_security.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS tech$lnk_index_security;
+CREATE TABLE tech$lnk_index_security
+  (
+    tech$load_id       INTEGER NOT NULL,
+    tech$hash_key      TEXT    NOT NULL,
+    tech$record_source TEXT    NOT NULL,
+    tech$load_dt       TEXT    NOT NULL,
+    tech$last_seen_dt  TEXT    NOT NULL,
+    trade_dt           TEXT    NOT NULL,
+    index_hash_key     TEXT    NOT NULL,
+    security_hash_key  TEXT    NOT NULL,
+    PRIMARY KEY(tech$hash_key),
+    UNIQUE(trade_dt, index_hash_key, security_hash_key)
+  )
+WITHOUT ROWID;
---
+++ db/script/etl/dds/lnk_index_security/sql/auxiliary/tech$lnk_index_security_truncate.sql
@@ -0,0 +1,1 @@
+DELETE FROM tech$lnk_index_security
