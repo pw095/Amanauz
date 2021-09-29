1979038fe0a46d3793b6f4a81800896f37fdf07d
Pavel Gnezdilov <pw095@fnbic.ru>
2021-09-29 22:41:57 +0300

    Исправить загрузку сущность lnk_currency_rate

--- db/script/etl/dds/lnk_currency_rate/sql/tech$lnk_currency_rate.sql
+++ db/script/etl/dds/lnk_currency_rate/sql/tech$lnk_currency_rate.sql
@@ -32,16 +32,32 @@ WITH
                                 trade_dt,
                                 crnc_code
                            FROM (SELECT
-                                        rate.tech$effective_dt,
-                                        rate.tech$last_seen_dt,
-                                        rate.trade_date                       AS trade_dt,
-                                        IFNULL(dict.iso_char_code, 'UNKNOWN') AS crnc_code
-                                   FROM src.foreign_currency_rate rate
-                                        JOIN
-                                        src.foreign_currency_dictionary dict
-                                            ON dict.id = rate.id
-                                           AND dict.tech$expiration_dt = '2999-12-31'
-                                  WHERE rate.tech$effective_dt >= :tech$effective_dt)))
+                                        tech$effective_dt,
+                                        tech$last_seen_dt,
+                                        trade_date                       AS trade_dt,
+                                        IFNULL(iso_char_code, 'UNKNOWN') AS crnc_code
+                                   FROM (SELECT
+                                                rate.tech$effective_dt,
+                                                rate.tech$last_seen_dt,
+                                                rate.trade_date,
+                                                dict.iso_char_code
+                                           FROM src.foreign_currency_rate rate
+                                                JOIN
+                                                src.foreign_currency_dictionary dict
+                                                    ON dict.id = rate.id
+                                                   AND rate.tech$expiration_dt BETWEEN dict.tech$effective_dt AND dict.tech$expiration_dt
+                                          WHERE rate.tech$effective_dt >= :tech$effective_dt
+                                          UNION ALL
+                                         SELECT
+                                                dict.tech$effective_dt,
+                                                dict.tech$last_seen_dt,
+                                                rate.trade_date         AS trade_dt,
+                                                dict.iso_char_code
+                                           FROM src.foreign_currency_dictionary dict
+                                                JOIN
+                                                src.foreign_currency_rate rate
+                                                    ON rate.id = dict.id
+                                                   AND dict.tech$expiration_dt BETWEEN rate.tech$effective_dt AND rate.tech$expiration_dt))))
           GROUP BY
                    tech$hash_key
      )
