d6fbca10e9673ce4d39b58b89c08b7f8846e3319
Pavel Gnezdilov <pw095@fnbic.ru>
2021-09-29 23:23:06 +0300

    Исправить загрузку сущности lnk_fin_report

--- db/script/etl/dds/lnk_fin_report/sql/tech$lnk_fin_report.sql
+++ db/script/etl/dds/lnk_fin_report/sql/tech$lnk_fin_report.sql
@@ -10,7 +10,7 @@ INSERT
     fs_code,
     emitent_hash_key,
     report_dt
-  )
+  )ы
 WITH
      w_pre AS
      (
@@ -35,22 +35,39 @@ WITH
                                 tech$last_seen_dt,
                                 '_' || IFNULL(CAST(currency      AS TEXT), '!@#$%^&*') ||
                                 '_' || IFNULL(CAST(fin_stmt_code AS TEXT), '!@#$%^&*') ||
-                                '_' || IFNULL(CAST(emitent_name  AS TEXT), '!@#$%^&*') ||
+                                '_' || IFNULL(CAST(full_name     AS TEXT), '!@#$%^&*') ||
                                 '_' || IFNULL(CAST(report_dt     AS TEXT), '!@#$%^&*') || '_' AS concat_value,
                                 currency,
                                 fin_stmt_code,
-                                emitent_name,
+                                full_name                                                     AS emitent_name,
                                 report_dt
                            FROM (SELECT
-                                        tech$effective_dt,
-                                        tech$last_seen_dt,
-                                        emitent_name,
-                                        currency,
-                                        report_dt,
-                                        fin_stmt_code
-                                   FROM src.emitent_fin_statement
+                                        fs.tech$effective_dt,
+                                        fs.tech$last_seen_dt,
+                                        emit.full_name,
+                                        fs.currency,
+                                        fs.report_dt,
+                                        fs.fin_stmt_code
+                                   FROM src.emitent_fin_statement fs
+                                        JOIN
+                                        src.master_data_emitent emit
+                                            ON emit.short_name = fs.emitent_name
+                                           AND fs.tech$expiration_dt BETWEEN emit.tech$effective_dt AND emit.tech$expiration_dt
+                                  UNION ALL
+                                 SELECT
+                                        emit.tech$effective_dt,
+                                        emit.tech$last_seen_dt,
+                                        emit.full_name,
+                                        fs.currency,
+                                        fs.report_dt,
+                                        fs.fin_stmt_code
+                                   FROM src.master_data_emitent emit
+                                        JOIN
+                                        src.emitent_fin_statement fs
+                                            ON fs.emitent_name = emit.short_name
+                                           AND emit.tech$expiration_dt BETWEEN fs.tech$effective_dt AND fs.tech$expiration_dt)
                                   WHERE 1 = 1
-                                     OR tech$effective_dt >= :tech$effective_dt)))
+                                     OR tech$effective_dt >= :tech$effective_dt))
           GROUP BY
                    tech$hash_key
      )
@@ -66,12 +83,8 @@ SELECT
        pre.report_dt
   FROM w_pre pre
        JOIN
-       src.master_data_emitent src_emit
-           ON src_emit.short_name = pre.emitent_name
-          AND src_emit.tech$expiration_dt = '2999-12-31'
-       JOIN
        hub_emitent hub_emit
-           ON hub_emit.code = src_emit.full_name
+           ON hub_emit.code = pre.emitent_name
  WHERE EXISTS(SELECT
                      NULL
                 FROM ref_currency crnc
