9219ea41c6a374d8dbcfc88779548f9d1cee7fdc
Pavel Gnezdilov <pw095@fnbic.ru>
2021-10-04 09:28:38 +0300

    Иправить заполнение таблицы tech$hub_board

--- db/script/etl/dds/hub_board/sql/tech$hub_board.sql
+++ db/script/etl/dds/hub_board/sql/tech$hub_board.sql
@@ -12,65 +12,59 @@ WITH
      w_pre AS
      (
          SELECT
-                sha1(board_id)         AS tech$hash_key,
-                MIN(tech$effective_dt) AS tech$load_dt,
-                MAX(tech$last_seen_dt) AS tech$last_seen_dt,
-                tech$record_source,
+                sha1(board_id)          AS tech$hash_key,
+                MIN(tech$effective_dt)  AS tech$load_dt,
+                MAX(tech$last_seen_dt)  AS tech$last_seen_dt,
+                MAX(tech$record_source) AS tech$record_source,
                 board_id
            FROM (SELECT
                         'moex.com'         AS tech$record_source,
                         tech$effective_dt,
-                        tech$expiration_dt,
                         tech$last_seen_dt,
                         board_id
                    FROM (SELECT
                                 tech$effective_dt,
-                                tech$expiration_dt,
                                 tech$last_seen_dt,
                                 board_id
                            FROM src.security_boards
                           UNION ALL
                          SELECT
                                 tech$effective_dt,
-                                tech$expiration_dt,
-                                tech$last_seen_dt,
-                                board_id
-                           FROM src.security_rate_bonds
-                          UNION ALL
-                         SELECT
-                                tech$effective_dt,
-                                tech$expiration_dt,
-                                tech$last_seen_dt,
-                                board_id
-                           FROM src.security_rate_shares
-                          UNION ALL
-                         SELECT
-                                tech$effective_dt,
-                                tech$expiration_dt,
                                 tech$last_seen_dt,
                                 board_id
                            FROM src.security_daily_info_bonds
                           UNION ALL
                          SELECT
                                 tech$effective_dt,
-                                tech$expiration_dt,
                                 tech$last_seen_dt,
                                 board_id
-                           FROM src.security_daily_info_shares)
+                           FROM src.security_daily_info_shares
+                          UNION ALL
+                         SELECT
+                                tech$effective_dt,
+                                tech$last_seen_dt,
+                                board_id
+                           FROM (SELECT
+                                        tech$effective_dt,
+                                        tech$last_seen_dt,
+                                        board_id
+                                   FROM src.security_rate_bonds
+                                  UNION ALL
+                                 SELECT
+                                        tech$effective_dt,
+                                        tech$last_seen_dt,
+                                        board_id
+                                   FROM src.security_rate_shares)
+                          WHERE tech$effective_dt >= :tech$effective_dt)
                   WHERE NULLIF(board_id, '') IS NOT NULL
                   UNION ALL
                  SELECT
                         'master_data'      AS tech$record_source,
                         tech$effective_dt,
-                        tech$expiration_dt,
                         tech$last_seen_dt,
                         board_id
                    FROM src.default_data_board)
-          WHERE (   1 = 1
-                 OR tech$effective_dt >= :tech$effective_dt)
-            AND tech$expiration_dt = '2999-12-31'
           GROUP BY
-                   tech$record_source,
                    board_id
      )
 SELECT
